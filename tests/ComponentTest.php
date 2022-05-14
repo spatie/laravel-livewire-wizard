c0f15b676f4f6ece15f5a751c55ba7e3ce4b95ae
Freek Van der Herten <freek@spatie.be>
2022-05-14 09:22:20 +0200

    wip

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -2,7 +2,12 @@
 
 use Livewire\Livewire;
 use Spatie\LivewireWizard\Tests\TestSupport\Components\MyWizardComponent;
+use Spatie\LivewireWizard\Tests\TestSupport\Components\Steps\FirstStepComponent;
 
 it('can render a wizard component', function() {
    Livewire::test(MyWizardComponent::class)->assertSuccessful();
+});
+
+it('can render a step component', function() {
+    Livewire::test(FirstStepComponent::class)->assertSuccessful();
 });
