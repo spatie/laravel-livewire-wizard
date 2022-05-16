2a3d6abb32536e34562b1b16b96feed558012bd2
freekmurze <freekmurze@users.noreply.github.com>
2022-05-16 07:35:13 +0000

    Fix styling

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -18,7 +18,7 @@ it('skips current step', function () {
        ->assertEmittedUp('nextStep');
 });
 
-it('can render the next step', function() {
+it('can render the next step', function () {
     $wizard = Livewire::test(MyWizardComponent::class)
         ->assertSuccessful()
         ->assertSee('first step');
@@ -28,8 +28,4 @@ it('can render the next step', function() {
         ->call('nextStep');
 
     $wizard->assertSee('second step');
-
-
-
-
 });
