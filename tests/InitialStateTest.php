93c9083fe2ae1ae476f67a489574d9ba76a6d8b5
freekmurze <freekmurze@users.noreply.github.com>
2022-05-16 15:47:19 +0000

    Fix styling

--- tests/InitialStateTest.php
+++ tests/InitialStateTest.php
@@ -19,19 +19,19 @@ it('can set initial state via a passed props', function () {
         ]);
 });
 
-it('can handle manually passed in state', function() {
+it('can handle manually passed in state', function () {
     $initialState = [
         'first-step' => [
             'order' => 123,
         ],
         'second-step' => [
             'counter' => 2,
-        ]
+        ],
     ];
 
     $this->wizard = Livewire::test(WizardWithInitialState::class, [
         'initialState' => $initialState,
-        'order' => 456 // will be ignored since initialState is passed
+        'order' => 456, // will be ignored since initialState is passed
     ])
         ->assertSuccessful()
         ->assertSet('allStepState', $initialState);
