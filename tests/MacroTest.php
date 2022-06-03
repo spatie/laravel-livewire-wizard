964e1d2c4dad4c629731f368b6381d56c20da618
ju5t <m@ju5t.nl>
2022-06-03 14:52:33 +0200

    tests: fetch state for steps without state

--- tests/MacroTest.php
+++ tests/MacroTest.php
@@ -3,6 +3,7 @@
 use Livewire\Livewire;
 use Spatie\LivewireWizard\Exceptions\StepDoesNotExist;
 use Spatie\LivewireWizard\Tests\TestSupport\Components\MyWizardComponent;
+use Spatie\LivewireWizard\Tests\TestSupport\Components\Steps\SecondStepComponent;
 
 beforeEach(function () {
     $this->wizard = Livewire::test(MyWizardComponent::class);
@@ -11,3 +12,9 @@ beforeEach(function () {
 it('throws an exception when state cannot be found', function () {
     $this->wizard->getStepState('three-thousand');
 })->throws(StepDoesNotExist::class, 'Step `three-thousand` does not exist.');
+
+it('gets state when no initial state is available', function () {
+    expect($this->wizard->getStepState(SecondStepComponent::class))
+        ->not
+        ->toThrow(StepDoesNotExist::class);
+});
