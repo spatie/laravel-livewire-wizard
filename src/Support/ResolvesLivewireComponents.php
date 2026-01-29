0c2f41b0315ae232816d5cae851ca17c311cf648
Tim Van Dijck <tim@spatie.be>
2026-01-29 15:16:07 +0100

    refactor: simplify trait to use Livewire 4 finder only

--- src/Support/ResolvesLivewireComponents.php
+++ src/Support/ResolvesLivewireComponents.php
@@ -2,30 +2,15 @@
 
 namespace Spatie\LivewireWizard\Support;
 
-use Livewire\Mechanisms\ComponentRegistry;
-
-/**
- * Provides Livewire component resolution that works with both Livewire 3 and 4.
- *
- * Livewire 4 replaced ComponentRegistry with a Finder (aliased as 'livewire.finder').
- */
 trait ResolvesLivewireComponents
 {
     private function componentName(string $name): string
     {
-        if (app()->has(ComponentRegistry::class)) {
-            return app(ComponentRegistry::class)->getName($name);
-        }
-
         return app('livewire.finder')->normalizeName($name);
     }
 
     private function componentClass(string $name): string
     {
-        if (app()->has(ComponentRegistry::class)) {
-            return app(ComponentRegistry::class)->getClass($name);
-        }
-
         return app('livewire.finder')->resolveClassComponentClassName($name);
     }
 }
