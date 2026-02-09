5a7e8df095c4a8b17d615826fbd51c7a868ce16d
Jérémy <babeuloula@gmail.com>
2026-02-09 16:24:11 +0100

    feat: add extra params to wizard testStep method (#140)

--- tests/TestStepMacroTest.php
+++ tests/TestStepMacroTest.php
@@ -2,7 +2,9 @@
 
 use Spatie\LivewireWizard\Tests\TestSupport\Components\MyWizardComponent;
 use Spatie\LivewireWizard\Tests\TestSupport\Components\Steps\FirstStepComponent;
+use Spatie\LivewireWizard\Tests\TestSupport\Components\Steps\MountStepComponent;
 use Spatie\LivewireWizard\Tests\TestSupport\Components\Steps\SecondStepComponent;
+use Spatie\LivewireWizard\Tests\TestSupport\Components\WizardWithMountComponent;
 
 it('can test a step without state', function () {
     MyWizardComponent::testStep(FirstStepComponent::class)
@@ -24,4 +26,15 @@ it('cannot test a step with invalid initial state', function () {
             'order' => 220,
         ],
     ]);
+})->throws(Exception::class);
+
+it('can test wizard with params', function () {
+    WizardWithMountComponent::testStep(MountStepComponent::class, params: [
+        'counter' => 999,
+    ])
+        ->assertSee('Mount step');
+});
+
+it('cant test wizard without params', function () {
+    WizardWithMountComponent::testStep(MountStepComponent::class);
 })->throws(Exception::class);
